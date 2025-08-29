16ebbeb3d19af723207b84f3c70fbc664035a1a1
Yasir Arafat Alif <yasirarafatalif1@gmail.com>
2025-08-29 11:51:14 +0600

    All functionality add

--- js/main.js
+++ js/main.js
@@ -8,7 +8,6 @@ function innerTextToInt(id) {
   return userInputValuue;
 }
 
-
 // "emergency-copy" section
 document
   .getElementById("emergency-copy")
@@ -21,17 +20,17 @@ document
     if (copy >= 5 || total < 0) {
       return alert("you dont have any coin ");
     }
-    alert("you copy thr number:999");
+    alert("you copy National Emergency Number:999");
     document.getElementById("coin").innerText = total1;
     document.getElementById("main-copy").innerText = total;
 
     const data = {
       name: "emergency-copy",
+      number: 999,
       date: new Date().toLocaleTimeString(),
     };
 
     history.push(data);
-    
   });
 // 'police-copy' section
 document.getElementById("police-copy").addEventListener("click", function () {
@@ -43,16 +42,17 @@ document.getElementById("police-copy").addEventListener("click", function () {
   if (copy >= 5 || total < 0) {
     return alert("you dont have any coin ");
   }
-  alert("you copy thr number:999");
+  alert("you copy Police Helpline Number:999");
   document.getElementById("coin").innerText = total1;
   document.getElementById("main-copy").innerText = total;
-    const data = {
-      name: "Police",
-      date: new Date().toLocaleTimeString(),
-    };
+  const data = {
+    name: "Police",
+    number: 999,
+    date: new Date().toLocaleTimeString(),
+  };
 
-    history.push(data);
-    historyCall();
+  history.push(data);
+  historyCall();
 });
 // fire copy section
 document.getElementById("fire-copy").addEventListener("click", function () {
@@ -67,16 +67,15 @@ document.getElementById("fire-copy").addEventListener("click", function () {
   alert("you copy thr number:999");
   document.getElementById("coin").innerText = total1;
   document.getElementById("main-copy").innerText = total;
-  
-    const data = {
-      name: "fire-copy",
-      date: new Date().toLocaleTimeString(),
-    };
 
-    history.push(data);
-    historyCall();
+  const data = {
+    name: "fire-copy",
+    number: 999,
+    date: new Date().toLocaleTimeString(),
+  };
 
-    
+  history.push(data);
+  historyCall();
 });
 // "ambulance-copy" section
 document
@@ -90,11 +89,12 @@ document
     if (copy >= 5 || total < 0) {
       return alert("you dont have any coin ");
     }
-    alert("you copy thr number:999");
+    alert("you copy Ambulance Service:1994-999999");
     document.getElementById("coin").innerText = total1;
     document.getElementById("main-copy").innerText = total;
-      const data = {
+    const data = {
       name: "Ambulance",
+      number: 1994 - 999999,
       date: new Date().toLocaleTimeString(),
     };
 
@@ -111,16 +111,17 @@ document.getElementById("women-copy").addEventListener("click", function () {
   if (copy >= 5 || total < 0) {
     return alert("you dont have any coin ");
   }
-  alert("you copy thr number:999");
+  alert("you copy Women & Child Helpline:109");
   document.getElementById("coin").innerText = total1;
   document.getElementById("main-copy").innerText = total;
-    const data = {
-      name: "Women",
-      date: new Date().toLocaleTimeString(),
-    };
+  const data = {
+    name: "Women",
+    number: 109,
+    date: new Date().toLocaleTimeString(),
+  };
 
-    history.push(data);
-    historyCall();
+  history.push(data);
+  historyCall();
 });
 // "anti-copy" section
 document.getElementById("anti-copy").addEventListener("click", function () {
@@ -132,16 +133,17 @@ document.getElementById("anti-copy").addEventListener("click", function () {
   if (copy >= 5 || total < 0) {
     return alert("you dont have any coin ");
   }
-  alert("you copy thr number:999");
+  alert("you copy Anti-Corruption Helpline:106");
   document.getElementById("coin").innerText = total1;
   document.getElementById("main-copy").innerText = total;
-    const data = {
-      name: "Anti",
-      date: new Date().toLocaleTimeString(),
-    };
+  const data = {
+    name: "Anti",
+    number: 106,
+    date: new Date().toLocaleTimeString(),
+  };
 
-    history.push(data);
-    historyCall();
+  history.push(data);
+  historyCall();
 });
 // ("electricity-copy")section
 document
@@ -155,15 +157,17 @@ document
     if (copy >= 5 || total < 0) {
       return alert("you dont have any coin ");
     }
-    alert("you copy thr number:999");
+    alert("you copy Electricity Helpline:16216");
     document.getElementById("coin").innerText = total1;
     document.getElementById("main-copy").innerText = total;
-      const data = {
+    const data = {
       name: "Electricity",
+      number: 12216,
       date: new Date().toLocaleTimeString(),
     };
 
     history.push(data);
+
     historyCall();
   });
 //('railway-copy') section
@@ -176,16 +180,17 @@ document.getElementById("railway-copy").addEventListener("click", function () {
   if (copy >= 5 || total < 0) {
     return alert("you dont have any coin ");
   }
-  alert("you copy thr number:999");
+  alert("you copy Bangladesh Railway Helpline:163");
   document.getElementById("coin").innerText = total1;
   document.getElementById("main-copy").innerText = total;
-    const data = {
-      name: "Railway",
-      date: new Date().toLocaleTimeString(),
-    };
+  const data = {
+    name: "Railway",
+    number: 163,
+    date: new Date().toLocaleTimeString(),
+  };
 
-    history.push(data);
-    historyCall();
+  history.push(data);
+  historyCall();
 });
 //"brac-copy" section
 document.getElementById("brac-copy").addEventListener("click", function () {
@@ -197,66 +202,88 @@ document.getElementById("brac-copy").addEventListener("click", function () {
   if (copy >= 5 || total < 0) {
     return alert("you dont have any coin ");
   }
-  alert("you copy thr number:999");
+  alert("you copy Brac Helpline:16445");
   document.getElementById("coin").innerText = total1;
   document.getElementById("main-copy").innerText = total;
-    const data = {
-      name: "Brac",
-      date: new Date().toLocaleTimeString(),
-    };
+  const data = {
+    name: "Brac",
+    number: "16445",
+    date: new Date().toLocaleTimeString(),
+  };
 
-    history.push(data);
-    historyCall();
+  history.push(data);
+  historyCall();
 });
 
-//call option event
 
 
-document.querySelectorAll(".call").forEach(function (e) {
-  e.addEventListener("click", function () {
-    const heart = innerTextToInt("heart");
-    const t = heart - 1;
-    if (t < 0) {
-      return alert("you cant call");
+
+// call repeat function
+function callMe(id, serviceName, serviceNumber) {
+  document.getElementById(id).addEventListener("click", function () {
+    const coin = innerTextToInt("coin");
+    const total1 = coin - 20;
+    if (total1 < 0) {
+      return alert("you dont have any coin ");
     }
-    alert("you can call");
-    document.getElementById("heart").innerText = t;
+
+    document.getElementById("coin").innerText = total1;
+    const headtext = document.querySelector(serviceName).innerText;
+
+    const head2text = document.querySelector(serviceNumber).innerText;
+    alert("you copy the number:" + head2text);
+
     const data = {
-      name: "call me",
+      name: headtext,
+      number: head2text,
       date: new Date().toLocaleTimeString(),
     };
 
     history.push(data);
     historyCall();
-    
+  });
+}
+
+document.getElementById("women-call").addEventListener("click", function () {
+  console.log(5);
+});
+//  all call
+callMe("emergency-call", ".national-call", ".national-number");
+callMe("police-call", ".police-call", ".police-number");
+callMe("fire-call", ".fire-call", ".fire-number");
+callMe("ambulance-call", ".ambulance-call", ".ambulance-number");
+callMe("women-call", ".men", ".women-number");
+callMe("anti-call", ".anti-call", ".anti-number");
+callMe("electricity-call", ".electricity-call", ".electricity-number");
+callMe("brac-call", ".brac-call", ".brac-number");
+
+callMe("railway-call", ".railway-call", ".railway-number");
+//call option event
+
+// heart icon section
+const heart = document.getElementsByClassName("fa-heart");
+document.querySelectorAll(".fa-heart").forEach(function (e) {
+  e.addEventListener("click", function () {
+    const heart = innerTextToInt("heart");
+    const heartTotal = heart + 1;
+    document.getElementById("heart").innerText = heartTotal;
   });
 });
 
+// this is call history section
+const transationContainer = document.getElementById("transation-container");
 
-
-
-const transatioContainer = document.getElementById("transation-container");
-// transatioContainer.innerHTML = "";
-
-
-// history.forEach((h,index)=>{
-
-// console.log(h);
-
-// });
-
-
-
-  function historyCall(){
-          for (const h of history) {
-                const div = document.createElement("div");
-  div.innerHTML = `
-              <div class="transation-container" id=" transation-container">
+function historyCall() {
+  transationContainer.innerHTML = "";
+  for (const h of history) {
+    const div = document.createElement("div");
+    div.innerHTML = `
+               <div class=" bg-[#F2F2F2] rounded-lg">
                   
               <div class="flex justify-between mt-4 p-2">
                  <div>
                   <h1 class="font-medium text-xl">${h.name}</h1>
-                  <p class="text-[#5C5C5C]">999</p>
+                  <p class="text-[#5C5C5C]">${h.number}</p>
                  </div>
                  <div>
                   <p>${h.date}</p>
@@ -264,10 +291,12 @@ const transatioContainer = document.getElementById("transation-container");
               </div>
             
               </div>
+              
     `;
-  transatioContainer.appendChild(div);
-          }
-  
+    transationContainer.appendChild(div);
   }
-
-
+}
+document.getElementById("clear").addEventListener('click',function(){
+  const transationContainer = document.getElementById("transation-container");
+  transationContainer.style.display='none';
+})
